14953ff58644bba3351682f8df888a23e6e2ea56
Timbo1993 <tim.kircher@gmx.de>
2017-06-20 13:55:48 +0200

    asf

--- src/commandtype/CommandType.java
+++ src/commandtype/CommandType.java
@@ -12,6 +12,10 @@
 package commandtype;
 
 import command.Command;
+import command.Direction;
+import command.Gear;
+import command.Pause;
+import command.Repetition;
 
 public class CommandType {
 	private String name;
@@ -20,17 +24,37 @@ public class CommandType {
 		this.name = myName;
 	}
 	
-	public Command createInstance(String myName){
+	public Command createInstance(){
+		switch(this.getName()){
 		
-		return null;		
+		case "Direction":
+			Direction instDir = new Direction(0);
+			return instDir;
+			
+		case "Gear":
+			Gear instGear = new Gear(0, 0);
+			return instGear;
+			
+		case "Pause":
+			Pause instPause = new Pause(0);
+			return instPause;
+			
+		case "Repetition":
+			Repetition instRep = new Repetition(0, 0);
+			return instRep;
+			
+		default:
+			return null;		
+	
+		}
 	}
 	
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String myName) {
-		this.name = myName;
-	}
+//	public void setName(String myName) {
+//		this.name = myName;
+//	}
 	
 }
--- src/controlmodel/ControlModel.java
+++ src/controlmodel/ControlModel.java
@@ -26,11 +26,14 @@ public class ControlModel {
 	}
 	
 	public ControlModel getInstance(){
-		return null;
+		return instance;
 	}
 	
 	public void createCommandTypes(){
-		
+		commandTypes[0] = new CommandType("Direction");
+		commandTypes[1] = new CommandType("Gear");
+		commandTypes[2] = new CommandType("Repetition");
+		commandTypes[3] = new CommandType("Pause");
 	}
 	
 //	public boolean load(File f){
@@ -41,12 +44,12 @@ public class ControlModel {
 //		return null;
 //	}
 	
-	public void commandPerformed(Command c){
+	public void commandPerformed(Command com){
 		
 	}
 	
 	public CommandList getControlProcess(){
-		return null;
+		return controlProcess;
 	}
 	
 }
